fa8c3f4c5b572b78e115f14f72b3f5ccedfa62ef
Raja Avinash <rajaavinash.azad@gmail.com>
2019-05-29 13:23:42 +0530

    css changes

--- htmlquestion/question5.js
+++ htmlquestion/question5.js
@@ -8,6 +8,14 @@ function load() {
         {title: 'Watches: Buy Wrist Watches for Men online at best prices in India', src: 'img/p5img.jpg'},
         {title: 'Automatic Watches - TISSOT® Canada', src: 'img/p6img.png'},
         {title: 'Moscone', src: 'img/p7img.jpg'},
+        {title: 'Frankie - Dark Sandalwood & Smoke - Minimalist Wooden Watch by JORD', src: 'img/p8img.png'},
+        {title: 'Mens Watches - Buy Watches for Men Online in India | Myntra', src: 'img/p1img.jpg'},
+        {title: 'Black Link Watch | Men\'s - 45mm | Classic Series | MVMT', src: 'img/p2img.jpg'},
+        {title: 'Black Tan Watch | Men\'s - 45mm | Classic Series | MVMT', src: 'img/p3img.jpg'},
+        {title: 'Watches - Buy Wrist Watches for Men & Women Online | Myntra', src: 'img/p4img.jpg'},
+        {title: 'Watches: Buy Wrist Watches for Men online at best prices in India', src: 'img/p5img.jpg'},
+        {title: 'Automatic Watches - TISSOT® Canada', src: 'img/p6img.png'},
+        {title: 'Moscone', src: 'img/p7img.jpg'},
         {title: 'Frankie - Dark Sandalwood & Smoke - Minimalist Wooden Watch by JORD', src: 'img/p8img.png'}
     ]
 
